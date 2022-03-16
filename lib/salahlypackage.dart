b204863b58a96e5a29ab072892d1ee9904920ce2
sergi-s <52626795+sergi-s@users.noreply.github.com>
2022-03-16 16:36:20 +0200

    update exports

--- lib/salahlypackage.dart
+++ lib/salahlypackage.dart
@@ -1,3 +1,32 @@
 library salahlypackage;
 
-export "src/salahly.dart";
+export "classes/models/car.dart";
+export "classes/models/client.dart";
+export "classes/models/location.dart";
+export "classes/models/mechanic.dart";
+export "classes/models/towProvider.dart";
+export "classes/models/workShop.dart";
+
+export "classes/firebase/firebase.dart";
+
+export "screens/login_signup/loginForm.dart";
+export "screens/login_signup/signupForm.dart";
+export "screens/login_signup/signupscreen.dart";
+
+export "screens/myLocation/getLocationComponent.dart";
+
+export "screens/splashScreen/splashScreen.dart";
+
+// export "utils/constants.dart";
+export "utils/validation.dart";
+
+export "widgets/location/divider.dart";
+export "widgets/location/drawer.dart";
+export "widgets/location/progressDialog.dart";
+export "widgets/location/whereToWidget.dart";
+
+export "widgets/login_signup/Input_container.dart";
+export "widgets/login_signup/Rounded_Bottom.dart";
+export "widgets/login_signup/Rounded_password.dart";
+export "widgets/login_signup/Rouned-Input.dart";
+
